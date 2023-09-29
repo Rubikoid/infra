de36bafb922883b853542f13ccb81131bcb8d794
Rubikoid <rubikoid@rubikoid.ru>
2023-09-29 05:38:24 +0300

    Part of split-dns

---
+++ nix/modules/system/other/split-dns.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+  networking.resolvconf.extraConfig = ''
+    unbound_conf=/etc/unbound-resolvconf.conf
+  '';
+
+  services.unbound = {
+    enable = true;
+    resolveLocalQueries = true;
+
+    settings = {
+      include = [
+        "/etc/unbound-resolvconf.conf"
+      ];
+      server = {
+        interface = [ "127.0.0.1" ];

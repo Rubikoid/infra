8ca6cd5cbb7c263d19bf79164b356da5bd8dce36
Rubikoid <rubikoid@rubikoid.ru>
2023-09-30 20:14:07 +0300

    Prepare dedic config

---
+++ nix/hosts/dedic/default.nix
@@ -0,0 +1,28 @@
+{ pkgs, config, inputs, lib, ... }:
+
+{
+  imports = with inputs.self.systemModules; [
+    ./hardware-configuration.nix
+
+    compact
+    locale
+    zsh
+
+    # ca
+    ca_rubikoid
+
+    # security
+    openssh
+    openssh-root-key
+  ];
+
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+}

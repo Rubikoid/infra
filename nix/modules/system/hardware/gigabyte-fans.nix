f78424156cee5461d4dd2e0f7c8425626db39002
Rubikoid <rubikoid@rubikoid.ru>
2023-11-05 16:38:25 +0300

    Some fix for gigabyte fans

--- nix/modules/system/hardware/gigabyte-fans.nix
+++ nix/modules/system/hardware/gigabyte-fans.nix
@@ -7,7 +7,7 @@
     kernelModules = [ "drivetemp" "coretemp" "it87" ];
 
     extraModprobeConfig = ''
-      options it87 ignore_resource_conflict=1 force_id=0x8689
+      options it87 ignore_resource_conflict=1
     '';
   };
 }

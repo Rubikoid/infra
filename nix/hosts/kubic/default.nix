c5ad7928a3b0c997d800d521eedae95fe4bd42e4
Rubikoid <rubikoid@rubikoid.ru>
2023-11-26 20:42:12 +0300

    Update kubic

--- nix/hosts/kubic/default.nix
+++ nix/hosts/kubic/default.nix
@@ -71,6 +71,41 @@
     jq
   ];
 
+  virtualisation = {
+    oci-containers.backend = "docker";
+
+    docker = {
+      enable = true;
+
+      storageDriver = "zfs";
+      daemon.settings = {
+        data-root = "/backup-drive/docker-data";
+      };
+    };
+  };
+
+  hardware.fancontrol = {
+    enable = true;
+
+    config = ''
+      INTERVAL=10
+      DEVPATH=hwmon6=devices/platform/it87.2624 hwmon7=devices/pci0000:00/0000:00:18.3
+      DEVNAME=hwmon6=it8689 hwmon7=k10temp
+      FCTEMPS=hwmon6/pwm5=hwmon7/temp1_input hwmon6/pwm1=hwmon7/temp1_input
+      FCFANS=hwmon6/pwm5=hwmon6/fan5_input hwmon6/pwm1=hwmon6/fan1_input
+      MINTEMP=hwmon6/pwm5=30 hwmon6/pwm1=30
+      MAXTEMP=hwmon6/pwm5=65 hwmon6/pwm1=65
+      MINSTART=hwmon6/pwm5=150 hwmon6/pwm1=150
+      MINSTOP=hwmon6/pwm5=0 hwmon6/pwm1=0
+    '';
+  };
+
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_14;
+    dataDir = "/backup-drive/data/psql//${config.services.postgresql.package.psqlSchema}";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave

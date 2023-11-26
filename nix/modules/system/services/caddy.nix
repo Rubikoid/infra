9d49c14de374d07c3e6cc4531b434c5ecb32770f
Rubikoid <rubikoid@rubikoid.ru>
2023-11-26 20:40:38 +0300

    Caddy

---
+++ nix/modules/system/services/caddy.nix
@@ -0,0 +1,30 @@
+{ lib, config, secrets, pkgs, ... }:
+{
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+  services.caddy = {
+    enable = true;
+
+    email = secrets.cert-email;
+    globalConfig = ''
+      # debug
+      servers {
+        trusted_proxies static ${secrets.trusted-proxy}
+      }
+    '';
+
+    extraConfig = ''
+      (stepssl) {
+        transport http {
+          tls_trusted_ca_certs ${config.sops.secrets."ca/rubikoid".path}
+        }
+      }
+
+      (stepssl_acme) {
+        tls cert+internal@rubikoid.ru {
+          ca ${secrets.pki.addr}
+          ca_root ${config.sops.secrets."ca/rubikoid".path}
+        }
+      }
+    '';
+  };
+}

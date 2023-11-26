e0f094357c679060bda86c89db7ccf44f94a6b36
Rubikoid <rubikoid@rubikoid.ru>
2023-11-26 20:41:16 +0300

    Fix ygg

--- nix/modules/system/yggdrasil.nix
+++ nix/modules/system/yggdrasil.nix
@@ -4,31 +4,54 @@ let
   scr = secrets.yggdrasil;
 in
 {
-  sops.secrets."yggdrasil.hjson" = {
-    sopsFile = config.deviceSecrets + "/yggdrasil.hjson";
-    format = "binary";
-  };
-
-  services.yggdrasil = {
-    enable = true;
-    openMulticastPort = lib.mkDefault true;
-    group = "wheel";
-    denyDhcpcdInterfaces = [ "ygg" ];
-
-    configFile = config.sops.secrets."yggdrasil.hjson".path;
-    settings = {
-      IfName = "ygg";
-
-      MulticastInterfaces = map
-        (pw: {
-          Regex = ".*";
-          Beacon = true;
-          Listen = true;
-          Port = 0;
-          Priority = 0;
-          Password = pw;
-        })
-        scr.passwords;
+  options.rubikoid.services.yggdrasil =
+    let
+      types = lib.types;
+    in
+    {
+      startMulticast = lib.mkOption {
+        type = types.bool;
+        default = true;
+      };
     };
-  };
+
+  config =
+    let
+      cfg = config.rubikoid.services.yggdrasil;
+    in
+    {
+      sops.secrets."yggdrasil.hjson" = {
+        sopsFile = secrets.deviceSecrets + "/yggdrasil.hjson";
+        format = "binary";
+      };
+
+      services.yggdrasil = {
+        enable = true;
+        openMulticastPort = cfg.startMulticast;
+        group = "wheel";
+        denyDhcpcdInterfaces = [ "ygg" ];
+
+        configFile = config.sops.secrets."yggdrasil.hjson".path;
+        settings = lib.recursiveUpdate
+          {
+            IfName = "ygg";
+
+            MulticastInterfaces =
+              if cfg.startMulticast
+              then
+                map
+                  (pw: {
+                    Regex = ".*";
+                    Beacon = true;
+                    Listen = true;
+                    Port = 0;
+                    Priority = 0;
+                    Password = pw;
+                  })
+                  scr.passwords
+              else [ ];
+          }
+          scr.settings;
+      };
+    };
 }

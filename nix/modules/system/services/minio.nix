8e16d641bbfc064b73b0d78252fefb2c81b3f8ae
Rubikoid <rubikoid@rubikoid.ru>
2023-11-26 18:53:41 +0300

    Add minio

---
+++ nix/modules/system/services/minio.nix
@@ -0,0 +1,87 @@
+{ lib, config, secrets, pkgs, ... }:
+{
+  options.rubikoid.services.minio =
+    let
+      types = lib.types;
+    in
+    {
+      keys = {
+        access = lib.mkOption {
+          type = types.str;
+          default = secrets.minio-keys.access;
+        };
+
+        secret = lib.mkOption {
+          type = types.str;
+          default = secrets.minio-keys.secret;
+        };
+      };
+      access = {
+        host = lib.mkOption {
+          type = types.str;
+          default = "127.0.0.1";
+        };
+
+        port = lib.mkOption {
+          type = types.port;
+          default = 9000;
+        };
+      };
+
+      console = {
+        host = lib.mkOption {
+          type = types.str;
+          default = "127.0.0.1";
+        };
+
+        port = lib.mkOption {
+          type = types.port;
+          default = 9001;
+        };
+      };
+
+      region = lib.mkOption {
+        type = types.str;
+        default = "ru-west-1";
+      };
+
+      dataFolders = lib.mkOption {
+        type = types.listOf types.str;
+        default = [
+          "/backup-drive/data/minio"
+        ];
+      };
+
+      caddyName = lib.mkOption {
+        type = types.str;
+        default = "minio-console";
+      };
+    };
+
+  config =
+    let
+      cfg = config.rubikoid.services.minio;
+    in
+    {
+      services.minio = {
+        enable = true;
+
+        accessKey = cfg.keys.access;
+        secretKey = cfg.keys.secret;
+
+        region = cfg.region;
+
+        listenAddress = "${cfg.access.host}:${toString cfg.access.port}";
+        consoleAddress = "${cfg.console.host}:${toString cfg.console.port}";
+
+        dataDir = cfg.dataFolders;
+      };
+
+      services.caddy.virtualHosts."${cfg.caddyName}.${secrets.dns.private}" = {
+        extraConfig = ''
+          reverse_proxy http://127.0.0.1:${toString cfg.console.port}
+          import stepssl_acme
+        '';
+      };
+    };
+}

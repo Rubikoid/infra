1593b928bb67637fbd9cd545ac590fd3199f899e
Rubikoid <rubikoid@rubikoid.ru>
2024-02-08 14:33:16 +0300

    Some immich fixes

--- nix/modules/system/services/garden/immich.nix
+++ nix/modules/system/services/garden/immich.nix
@@ -27,25 +27,7 @@
       env = { };
     in
     {
-      systemd.services.init-filerun-network-and-files = {
-        description = "Create the network bridge for Immich.";
-        after = [ "network.target" ];
-        wantedBy = [ "multi-user.target" ];
-
-        serviceConfig.Type = "oneshot";
-        script =
-          let dockercli = "${config.virtualisation.docker.package}/bin/docker";
-          in
-          ''
-            # immich-net network
-            check=$(${dockercli} network ls | grep "immich-net" || true)
-            if [ -z "$check" ]; then
-              ${dockercli} network create immich-net
-            else
-              echo "immich-net already exists in docker"
-            fi
-          '';
-      };
+      systemd.services.docker-network-immich = pkgs.my-lib.mkDockerNet config "immich";
 
       virtualisation.oci-containers.containers = {
         immich = {
@@ -62,23 +44,23 @@
             PUID = "1000";
             PGID = "1000";
             TZ = "Europe/Moscow"; # Change this to your timezone
-            DB_HOSTNAME = "postgres14";
+            DB_HOSTNAME = "immich-postgres14";
             DB_USERNAME = "postgres";
             DB_PASSWORD = "postgres";
             DB_DATABASE_NAME = "immich";
-            REDIS_HOSTNAME = "redis";
+            REDIS_HOSTNAME = "immich-redis";
           };
           extraOptions = [ "--network=immich-net" "--gpus=all" ];
         };
 
-        redis = {
+        immich-redis = {
           autoStart = true;
           image = "redis";
           # ports = [ "127.0.0.1:6379:6379" ];
           extraOptions = [ "--network=immich-net" ];
         };
 
-        postgres14 = {
+        immich-postgres14 = {
           autoStart = true;
           image = "postgres:14";
           # ports = [ "5432:5432" ];

b616b48914045d401432f020aa96b8a2c3742819
Rubikoid <rubikoid@rubikoid.ru>
2024-04-09 18:58:35 +0300

    Add r7-wsl

---
+++ nix/hosts/r7-wsl/default.nix
@@ -0,0 +1,33 @@
+{ pkgs, config, secrets, inputs, lib, ... }:
+
+{
+  imports = with inputs.self.systemModules; [
+    zsh
+    zsh-config
+
+    # ca
+    ca_rubikoid
+  ];
+
+  environment.systemPackages = with pkgs; [
+    bash
+    vim
+  ];
+
+  wsl = {
+    enable = true;
+    defaultUser = "rubikoid";
+    nativeSystemd = false;
+
+    wslConf = {
+      network.generateResolvConf = false;
+    };
+  };
+
+  networking.nameservers = [
+    "192.168.1.107"
+  ];
+
+  nixpkgs.hostPlatform = lib.mkDefault config.system-arch-name;
+  system.stateVersion = "24.05"; # Did you read the comment?
+}

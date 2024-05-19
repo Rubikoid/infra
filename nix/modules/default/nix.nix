67be5895dc4c909a666f3e08694e5bf781605ddf
Rubikoid <rubikoid@rubikoid.ru>
2024-05-19 18:34:06 +0300

    modules: make default nix module a little better

--- nix/modules/default/nix.nix
+++ nix/modules/default/nix.nix
@@ -14,7 +14,8 @@
       # };
     };
 
-    # idk wtf is it, but sounds good;
+    # linking hardlinks inside store
+    # good thing
     optimise.automatic = true;
 
     gc = {
@@ -30,13 +31,19 @@
         "flakes"
         "repl-flake"
       ];
+
+      # чтобы работало, надо либо чтобы юзер в trusted-users, либо репа в trusted-substituters
+      # я добавляю и то и туда, чтобы работало гарантированно. 
       substituters = [
         "https://${secrets.harmonia.addr}"
-        "https://cache.nixos.org"
+        # "https://cache.nixos.org" # а это больше не добавляю, потому что оно дополняет, а не заменяет, как выяснилось
+      ];
+      trusted-substituters = [
+        "https://${secrets.harmonia.addr}"
       ];
       trusted-public-keys = [
         "${secrets.harmonia.addr}:${secrets.harmonia.key}"
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        # "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
     };
   };

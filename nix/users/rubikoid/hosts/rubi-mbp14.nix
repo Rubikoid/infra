e85806b1f3da2ab983395555aff7227fbdb591c6
Rubikoid <rubikoid@rubikoid.ru>
2024-06-05 19:26:27 +0300

    user-mbp14: Migrate pkgs from brew

--- nix/users/rubikoid/hosts/rubi-mbp14.nix
+++ nix/users/rubikoid/hosts/rubi-mbp14.nix
@@ -3,10 +3,24 @@
   imports = with inputs.self.userModules; [
     sops
     helix
+    python
   ];
 
   home.packages = with pkgs; [
+    ansible
+    ffmpeg
+    colima
+    lima
+    yt-dlp
+    graphviz
+    binwalk
+    imagemagick
+    qemu
 
+    # glib, gobject-introspection, harfbuzz, ldc, librsvg, lima, llvm, python-tk@3.12, qt@5 and yt-dlp
+    # 
+    nil
+    nixpkgs-fmt
   ];
 }
 

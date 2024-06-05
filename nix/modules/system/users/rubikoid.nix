f0e4ce428a1c207d2daaf58b7d8b70bd3a3eaada
Rubikoid <rubikoid@rubikoid.ru>
2024-06-05 19:27:06 +0300

    Fix generic user module

--- nix/modules/system/users/rubikoid.nix
+++ nix/modules/system/users/rubikoid.nix
@@ -2,23 +2,21 @@
 
 {
   # Setup users
-  users.users = {
-    rubikoid = lib.mkMerge [
-      {
-        openssh.authorizedKeys.keys = [ secrets.ssh.rubikoid.main ];
-      }
-      (lib.mkIf (mode == "NixOS") {
-        isNormalUser = true;
-        useDefaultShell = true;
+  users.users.rubikoid = lib.mkMerge [
+    {
+      openssh.authorizedKeys.keys = [ secrets.ssh.rubikoid.main ];
+    }
+    (lib.mkIf (mode == "NixOS") {
+      isNormalUser = true;
+      useDefaultShell = true;
 
-        extraGroups = [ "wheel" "docker" "tss" ];
-      })
-      (lib.mkIf (mode == "Darwin") {
-        # https://github.com/nix-community/home-manager/issues/4026
-        # https://github.com/LnL7/nix-darwin/issues/682
-        # ????
-        home = "/Users/rubikoid";
-      })
-    ];
-  };
+      extraGroups = [ "wheel" "docker" "tss" ];
+    })
+    (lib.mkIf (mode == "Darwin") {
+      # https://github.com/nix-community/home-manager/issues/4026
+      # https://github.com/LnL7/nix-darwin/issues/682
+      # ????
+      home = "/Users/rubikoid";
+    })
+  ];
 }

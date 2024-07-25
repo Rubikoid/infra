2223160de30e1becfb8a43a5edef6cbeb8f2d13d
Rubikoid <rubikoid@rubikoid.ru>
2024-07-25 03:12:11 +0300

    Update openssh setup

--- nix/modules/system/security/openssh-root-key.nix
+++ nix/modules/system/security/openssh-root-key.nix
@@ -2,5 +2,8 @@
 
 {
   users.users.root.openssh.authorizedKeys.keys = [ secrets.ssh.rubikoid.main ];
-  services.openssh.settings.PermitRootLogin = lib.mkDefault "yes";
+  services.openssh.settings = {
+    PasswordAuthentication = lib.mkDefault false;
+    PermitRootLogin = lib.mkDefault "yes";
+  };
 }

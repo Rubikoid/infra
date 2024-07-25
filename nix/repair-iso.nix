730165d15f6f9d56f8ff4f56969e4fc293ae3100
Rubikoid <rubikoid@rubikoid.ru>
2024-07-25 03:11:17 +0300

    Move repair iso to separate file

---
+++ nix/repair-iso.nix
@@ -0,0 +1,78 @@
+{ inputs, lib, pkgs, ... }:
+let
+  hostname = "kubic";
+  system = lib.readSystem hostname;
+in
+inputs.nixos-generators.nixosGenerate {
+  inherit pkgs system;
+  modules = (with inputs.self.systemModules; [
+    ca_rubikoid
+
+    ntfs
+    zfs
+
+    openssh
+    openssh-root-key
+
+    gigabyte-fans
+
+    smartd
+  ]) ++
+  [
+    (import ./modules/default/options.nix)
+    (import ./modules/base-system.nix)
+    (import ./modules/base-system-linux.nix)
+    {
+      system-arch-name = system;
+      device = hostname;
+      isWSL = false;
+    }
+    {
+      nix = {
+        package = pkgs.nix;
+        registry = {
+          nixpkgs.flake = inputs.nixpkgs;
+          n.flake = inputs.nixpkgs;
+        };
+        settings = {
+          experimental-features = [
+            "nix-command"
+            "flakes"
+            "repl-flake"
+          ];
+        };
+      };
+    }
+    {
+      boot.supportedFilesystems = [ "exfat" "f2fs" "fat8" "fat16" "fat32" ];
+
+      networking.hostId = (inputs.self.secrets.secretsBuilder hostname).zfsHostId; # need by zfs
+      # systemd.services.sshd.wantedBy = lib.mkForce [ "multi-user.target" ];
+      services.zfs.autoScrub.enable = false;
+
+      # auto detect type of disk
+      # disable auto offline tests (they are slow and we don't need them now)
+      # check health
+      services.smartd.defaults.monitored = "-d auto -o off -H -l selftest -l selfteststs -l error -l xerror -f -t -C 197 -U 198";
+
+      environment.systemPackages = with pkgs; [
+        tmux
+        btop
+        htop
+        rsync
+        lm_sensors
+      ];
+
+      system.stateVersion = "23.11";
+    }
+  ];
+  format = "install-iso";
+  specialArgs = {
+    inherit inputs;
+
+    secretsModule = inputs.self.secrets.nixosModules.default;
+    secrets = inputs.self.secrets.secretsBuilder hostname;
+
+    mode = "NixOS";
+  };
+}

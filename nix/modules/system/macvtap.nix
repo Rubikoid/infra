50f8b021b76f8fe9f0b305e170a75d99c20edd94
Rubikoid <rubikoid@rubikoid.ru>
2024-08-02 21:24:51 +0300

    MicroVM things

---
+++ nix/modules/system/macvtap.nix
@@ -0,0 +1,3 @@
+{
+  networking.dhcpcd.denyInterfaces = [ "vm1@enp6s0" "vm1" "vm1@*" "vm1*" ];
+}
---
+++ nix/modules/system/microvm.nix
@@ -0,0 +1,48 @@
+{ lib, config, secrets, pkgs, inputs, my-lib, ... }:
+
+let
+  types = lib.types;
+  cfg = config.rubikoid.microvm;
+in
+{
+  imports = [
+    inputs.microvm.nixosModules.host
+  ];
+
+  options.rubikoid.microvm = {
+    vms = lib.mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+    };
+  };
+
+  config = {
+    microvm.vms =
+      let
+        enabledVMFilter = hostname: builtins.elem hostname cfg.vms;
+      in
+      lib.genAttrs (builtins.filter enabledVMFilter inputs.self.raw_vms) (hostname: {
+        # TODO: proper pkgs... i think...
+        inherit pkgs;
+        config = {
+          imports = builtins.attrValues inputs.self.defaultModules ++ [
+            ../base-system.nix
+            ../base-system-vm.nix
+            ../base-system-linux.nix
+            (../../vms + "/${hostname}")
+          ];
+
+          system-arch-name = "x86_64-linux";
+          device = hostname;
+          isWSL = false;
+        };
+        specialArgs = {
+          inherit inputs my-lib;
+
+          secretsModule = inputs.self.secrets.nixosModules.default;
+          secrets = inputs.self.secrets.secretsBuilder hostname;
+          mode = "NixOS";
+        };
+      });
+  };
+}

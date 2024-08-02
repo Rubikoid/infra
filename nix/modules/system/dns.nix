ec139e6bd3f16d102475c6c097424afc368448aa
Rubikoid <rubikoid@rubikoid.ru>
2024-08-02 17:41:58 +0300

    Prepare HTTP and DNS modules

---
+++ nix/modules/system/dns.nix
@@ -0,0 +1,97 @@
+{ lib, config, secrets, pkgs, inputs, ... }:
+
+let
+  types = lib.types;
+  cfg = config.rubikoid.dns;
+in
+{
+  imports = [ inputs.nixos-dns.nixosModules.dns ];
+
+  options.rubikoid.dns = {
+    rootZone = lib.mkOption {
+      type = types.str;
+    };
+
+    nodes = lib.mkOption {
+      type = types.attrsOf (types.submodule ({ name, ... }: {
+        options = {
+          name = lib.mkOption {
+            type = types.str;
+            default = name;
+          };
+
+          v4 = lib.mkOption {
+            type = types.listOf types.str;
+            default = [ ];
+          };
+
+          v6 = lib.mkOption {
+            type = types.listOf types.str;
+            default = [ ];
+          };
+
+          cname = lib.mkOption {
+            type = types.listOf types.str;
+            default = [ ];
+          };
+        };
+      }));
+      default = { };
+    };
+
+    services = lib.mkOption {
+      type = types.attrsOf types.str;
+      default = { };
+    };
+  };
+
+  config =
+    let
+      nodeFQDN = nodeName: "${nodeName}.nodes.${cfg.rootZone}";
+      serviceFQDN = serviceName: "${serviceName}.${cfg.rootZone}";
+    in
+    {
+      rubikoid.dns = secrets.dns.data;
+
+      # NixOS-DNS
+      networking.domains = lib.mkMerge ([
+        {
+          enable = true;
+          defaultTTL = 86400; # 24h
+          baseDomains.${cfg.rootZone} = { };
+        }
+      ] ++ (builtins.map
+        (node:
+          let
+            name = node.name;
+            fqdn = nodeFQDN name;
+            cnameRecords = builtins.map
+              (cn: {
+                name = nodeFQDN cn;
+                value = { cname.data = fqdn; };
+              })
+              node.cname;
+          in
+          {
+            subDomains = {
+              ${fqdn} = {
+                a.data = node.v4;
+                aaaa.data = node.v6;
+              };
+            } // (builtins.listToAttrs cnameRecords);
+          })
+        (builtins.attrValues cfg.nodes)
+      )
+      ++ (lib.mapAttrsToList
+        (service: nodeName:
+          let
+            fqdn = serviceFQDN service;
+            node = nodeFQDN nodeName;
+          in
+          {
+            subDomains.${fqdn}.cname.data = node;
+          })
+        cfg.services
+      ));
+    };
+}
---
+++ nix/modules/system/http.nix
@@ -0,0 +1,64 @@
+{ lib, config, secrets, my-lib, ... }:
+
+let
+  types = lib.types;
+  cfg = config.rubikoid.http;
+in
+{
+  options.rubikoid.http = {
+    services = lib.mkOption {
+      type = types.attrsOf (types.submodule ({ name, ... }:
+        let
+          service = cfg.services.${name};
+        in
+        {
+          options = {
+            name = lib.mkOption {
+              type = types.str;
+              default = name;
+            };
+
+            host = lib.mkOption {
+              type = types.str;
+              default = config.networking.hostName;
+            };
+
+            port = lib.mkOption {
+              type = types.port;
+            };
+
+            hostOnHost = lib.mkOption {
+              type = types.str;
+              default = "127.0.0.1";
+            };
+
+            caddyConfig = lib.mkOption {
+              type = types.str;
+              default = ''
+                reverse_proxy http://${service.hostOnHost}:${toString service.port}
+                import stepssl_acme
+              '';
+            };
+
+            fqdn = lib.mkOption {
+              type = types.str;
+              default = "${service.name}.${config.rubikoid.dns.rootZone}";
+            };
+
+          };
+        }));
+      default = { };
+    };
+  };
+
+  config = {
+    rubikoid.dns.services = (lib.mapAttrs' (name: service: lib.nameValuePair service.name service.host) cfg.services);
+    services.caddy.virtualHosts = (lib.mapAttrs'
+      (name: service:
+        lib.nameValuePair
+          service.fqdn
+          { extraConfig = service.caddyConfig; }
+      )
+      cfg.services);
+  };
+}

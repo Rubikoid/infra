b467f17abb3d966cef0d076754884a9164c7b402
Rubikoid <rubikoid@rubikoid.ru>
2024-11-16 00:19:46 +0300

    Add patched nixfmt to pkgs

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,4 +1,5 @@
 inputs: final: prev: rec {
   mastodon-glitch = final.callPackage ./pkgs/mastodon/default.nix { };
   mongodb-ce = final.callPackage ./pkgs/mongodb-ce.nix { };
+  nixfmt-rubi-style = final.callPackage ./pkgs/nixfmt-patched.nix { };
 }

767d55ec5afe498cf763e06684c7db722c675a75
Rubikoid <rubikoid@rubikoid.ru>
2024-12-30 22:42:05 +0300

    dev

--- nix/pkgs/nixfmt-patched.nix
+++ nix/pkgs/nixfmt-patched.nix
@@ -1,14 +1,15 @@
-{ pkgs
-, lib
-, ...
-}: 
-let 
+{
+  pkgs,
+  lib,
+  ...
+}:
+let
   patch = pkgs.fetchpatch2 {
     url = "https://github.com/Rubikoid/nixfmt/pull/1.patch";
     sha256 = "sha256-0x9VBFuQtc36g4LSRZanb/9XJ6fni6sCiWQyxue8pog=";
   };
-in 
+in
 pkgs.nixfmt-rfc-style.overrideAttrs (old: {
-    pname = "nixfmt-rubi-style";
-    patches = (old.patches or []) ++ [patch];
- })
+  pname = "nixfmt-rubi-style";
+  patches = (old.patches or [ ]) ++ [ patch ];
+})

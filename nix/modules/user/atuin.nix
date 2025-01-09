5a65e1f2f35f9dd093f7590cbf535deaaa4cb879
Rubikoid <rubikoid@rubikoid.ru>
2025-01-10 02:36:58 +0300

    Atuin devs fucking crazy.

--- nix/modules/user/atuin.nix
+++ nix/modules/user/atuin.nix
@@ -1,9 +1,9 @@
 { secrets, config, ... }:
 {
-  sops.secrets."atuin" = {
-    sopsFile = secrets.deviceSecrets + "/secrets.yaml";
-    # mode = "0400";
-  };
+  # sops.secrets."atuin" = {
+  #   sopsFile = secrets.deviceSecrets + "/secrets.yaml";
+  #   # mode = "0400";
+  # };
 
   programs.atuin = {
     enable = true;
@@ -18,10 +18,15 @@
       filter_mode = "global";
       filter_mode_shell_up_key_binding = "session";
 
-      key_path = config.sops.secrets."atuin".path;
+      # WTF: atuin have cringe imperative shit
+      # https://github.com/atuinsh/atuin/issues/2479#issuecomment-2528215934
+      # key_path = config.sops.secrets."atuin".path;
 
-      sync_address = "https://atuin.${secrets.dns.private}";
+      sync.records = true;
+      auto_sync = true;
       sync_frequency = "10m";
+      sync_address = "https://atuin.${secrets.dns.private}";
+
       dialect = "uk";
       update_check = false;
     };

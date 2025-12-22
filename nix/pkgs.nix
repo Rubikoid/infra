f163698bd6edaee420a965a86699d3bf8056534f
Rubikoid <rubikoid@rubikoid.ru>
2025-12-23 02:45:26 +0300

    Add openthread-border-router Stolen from https://github.com/NixOS/nixpkgs/pull/332296 b/c it's not merged ;(

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,4 +1,5 @@
 inputs: final: prev: rec {
   mastodon-glitch = final.callPackage ./pkgs/mastodon/default.nix { };
   mongodb-ce = final.callPackage ./pkgs/mongodb-ce.nix { };
+  openthread-border-router = final.callPackage ./pkgs/openthread-border-router { };
 }
